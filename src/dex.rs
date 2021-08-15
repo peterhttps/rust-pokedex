360e960ba7f9678c89681f2dbd950b8e70fcbd0d
Pedro Lucas <plrf100@gmail.com>
2021-08-15 12:45:58 -0300

    git(start): starting repository

---
+++ src/dex.rs
@@ -0,0 +1,62 @@
+use std::io;
+use colored::*;
+
+#[derive(Debug, Clone)]
+pub struct Pokemon {
+    pub name: String,
+    pub id: u32,
+    pub main_type: String, 
+}
+
+pub struct Pokedex {
+  pub list: Vec<Pokemon>
+}
+
+#[allow(dead_code)]
+impl Pokedex {
+  pub fn new() -> Pokedex {
+    Pokedex { list: [].to_vec() }
+  }
+
+  pub fn add_to_pokedex(&mut self) {
+    let mut pokemon_name = String::new();
+    let mut pokemon_id = String::new();
+    let mut pokemon_type = String::new();
+
+    println!("Type the pokemon name: ");
+    io::stdin().read_line(&mut pokemon_name).expect("Failed to read message");
+    let pokemon_name = pokemon_name.trim();
+
+    println!("Type the pokemon id: ");
+    io::stdin().read_line(&mut pokemon_id).expect("Failed to read message");
+    let pokemon_id = match pokemon_id.trim().parse() {
+      Ok(num) => num,
+      Err(_) => 0
+    };
+
+    println!("Type the pokemon main type: ");
+    io::stdin().read_line(&mut pokemon_type).expect("Failed to read message");
+    let pokemon_type = pokemon_type.trim();
+
+    self.list.push(Pokemon {
+      name: pokemon_name.to_string(),
+      id: pokemon_id,
+      main_type: pokemon_type.to_string()
+    });
+
+    println!();
+  }
+
+  pub fn print_pokedex(&self) {
+
+    for pokemon in self.list.iter() {
+      println!("#################");
+      println!("Name: {}", pokemon.name.green());
+      println!("ID: {}", pokemon.id.to_string().green());
+      println!("Main type: {}", pokemon.main_type.green());
+      println!("#################");
+      println!();
+    }
+
+  }
+}
---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use std::io;
+mod dex;
+use crate::dex::*;
+
+fn main() {
+    println!();
+    println!("###### POKEDEX ######");
+    println!();
+
+    let mut pokedex = Pokedex::new();
+
+    loop {
+      println!("What you wanna do? \n1. Add Pokemon\n2. View all pokemons\n3. Quit");
+
+      let mut choice = String::new();
+      io::stdin().read_line(&mut choice).expect("Failed to read message");
+      let choice = match choice.trim().parse() {
+        Ok(num) => num,
+        Err(_) => 3
+      };
+
+      println!();
+
+      if choice == 1 {
+        pokedex.add_to_pokedex();
+      } else if choice == 2 {
+        pokedex.print_pokedex();
+      } else if choice == 3 {
+        break;
+      } else {
+        break;
+      }
+    }
+
+
+}
